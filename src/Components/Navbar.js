1cf0b25a2e26c93fa842c774daecaa3327a47969
B.M.Sudhamsh <130533943+meghaneelsudhamsh@users.noreply.github.com>
2024-07-12 09:49:49 +0530

    second commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,3 @@
-// Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,38 +6,39 @@ import './Navbar.css';
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="navbar-left">
-        <Link to="/home" className="navbar-logo">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link to="/home" className="navbar-brand">
           <img src="./images/logo.jpg" alt="Logo" className="logo" />
         </Link>
-        <ul className="navbar-links">
-          <li>
-            <Link to="/home">
-              <i className="bi bi-house-fill"></i>
-            </Link>
-          </li>
-          <li>
-            <Link to="/chatbot"><i className="bi bi-robot"></i>-Bot</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/help">Help</Link>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-right">
-        <button className="navbar-button">
-          <Link to="/login">Login</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <button className="navbar-button">
-          <Link to="/signup">Signup</Link>
-        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link to="/home" className="nav-link">
+                <i className="bi bi-house-fill"></i>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/chatbot" className="nav-link"><i className="bi bi-robot"></i>-Bot</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact" className="nav-link">Contact</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/help" className="nav-link">Help</Link>
+            </li>
+          </ul>
+          <div className="d-flex">
+            <Link to="/login" className="btn btn-outline-primary me-2">Login</Link>
+            <Link to="/signup" className="btn btn-outline-primary me-2">Signup</Link>
+          </div>
+        </div>
       </div>
     </nav>
   );
